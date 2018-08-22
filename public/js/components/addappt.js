4987f44c77c4f01c0ee09ca4afa9bd510e0985f2
shawnc8160 <shawnc8160@gmail.com>
2018-08-22 11:18:20 -0700

    able to toggle between creating category and selecting category

--- public/js/components/addappt.js
+++ public/js/components/addappt.js
@@ -3,7 +3,8 @@ class AddAppt extends React.Component {
     super(props)
     this.state = {
       startDate: null,
-      endDate: null
+      endDate: null,
+      createCategory: false
     }
   }
   handleChange = (date) => {
@@ -14,18 +15,23 @@ class AddAppt extends React.Component {
   renderForm = () => {
     return (
       <div class="form">
-        <div class="field">
-          <label class="label is-small">Title</label>
-          <div class="control has-icons-left has-icons-right">
-            <input class="input is-small" type="email" placeholder="Normal" />
+        <div class="field field-section">
+          <label class="label is-small">
             <span class="icon is-small is-left">
               <i class="far fa-calendar-plus"></i>
-            </span>
+            </span>Title</label>
+          <div class="control">
+            <input class="input is-small" type="text" placeholder="Title for appointment" />
           </div>
         </div>
-        <div class="datefield">
+        <div class="datefield field-section">
           <div class="field">
-            <label class="label is-small">Start Date</label>
+            <label class="label is-small">
+              <span class="icon is-small is-left">
+                <i class="far fa-clock"></i>
+              </span>
+              Start Date
+            </label>
             <DatePicker
               className="date"
               placeholderText="Start Date and Time"
@@ -42,7 +48,12 @@ class AddAppt extends React.Component {
             />
           </div>
           <div class="field">
-            <label class="label is-small">End Date</label>
+            <label class="label is-small">
+              <span class="icon is-small is-left">
+                <i class="far fa-clock"></i>
+              </span>
+              End Date
+            </label>
             <DatePicker
               className="date"
               placeholderText="End Date and Time"
@@ -59,22 +70,68 @@ class AddAppt extends React.Component {
             />
           </div>
         </div>
-        <div class="field">
-          <label class="label is-small">Choose Category</label>
-          <p class="control is-small">
-            <span class="select is-small">
-              <select>
-                <option>Category1</option>
-                <option>Category2</option>
-                <option>Category3</option>
-              </select>
+        <div class="field field-section-top">
+          <label class="label is-small">
+            <span class="icon is-small is-left">
+              <i class="far fa-folder-open"></i>
             </span>
-          </p>
-          <p class="control">
-            <a class="button is-small">
-              Create New Category
-            </a>
-          </p>
+            Category
+          </label>
+
+          {
+            (!this.state.createCategory)
+            ? <div class="field is-grouped">
+                <p class="control is-small">
+                  <span class="select is-small">
+                    <select>
+                      <option value="General" disabled selected>Select category</option>
+                      <option>Work</option>
+                      <option>Other</option>
+                    </select>
+                  </span>
+                </p>
+                <div class="form-divider"> OR </div>
+                <p class="control">
+                  <a class="button is-small" onClick={() => {
+                      this.setState({
+                        createCategory: !this.state.createCategory
+                      })
+                    }}>
+                    Create New Category
+                  </a>
+                </p>
+              </div>
+            : <div class="field is-grouped">
+                <p class="control">
+                  <input class="input is-small" type="text" placeholder="New category name" />
+                </p>
+
+                <p class="control is-small">
+                  <span class="select is-small">
+                    <select>
+                      <option value="Blue" disabled selected>Select color</option>
+                      <option>Red</option>
+                      <option>Green</option>
+                      <option>Blue</option>
+                    </select>
+                  </span>
+                </p>
+
+                <p class="control">
+
+                  <i class="far fa-check-circle icon-button yes"></i>
+
+                </p>
+
+                <p class="control">
+                  <i class="fas fa-ban icon-button no" onClick={() => {
+                      this.setState({
+                        createCategory: !this.state.createCategory
+                      })
+                    }}></i>
+                </p>
+              </div>
+            }
         </div>
 
       </div>
